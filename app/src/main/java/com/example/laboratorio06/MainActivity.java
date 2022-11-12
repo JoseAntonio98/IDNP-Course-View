7167988786f0e7c0d1a157dc94cdc40c185b7fd8
Joel Gutiérrez <64146055+joel123-gothub@users.noreply.github.com>
2022-11-12 13:25:26 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/laboratorio06/MainActivity.java
+++ app/src/main/java/com/example/laboratorio06/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.laboratorio06;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void goToGraficoBarrasActivity(View view) {
+        Intent graf1Intent = new Intent(this, GraficoBarras.class);
+        startActivity(graf1Intent);
+    }
+
+    public void goToGraficoCircularActivity(View view) {
+        Intent graf2Intent = new Intent(this, GraficoCircular.class);
+        startActivity(graf2Intent);
     }
 }
